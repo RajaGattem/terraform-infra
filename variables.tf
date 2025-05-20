550a503e66f011a503aafa2ed0818f2de217c22d
RajaGattem <raja.gattem@fissionlabs.com>
2025-05-20 12:19:56 +0530

    Initial VPC and EC2 setup

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
